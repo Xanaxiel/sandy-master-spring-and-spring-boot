9d673b0276fa0ec733a2cecfaf311d7d79518169
Xanaxiel <x.anaxiel-github@yahoo.com>
2023-10-11 21:37:09 +0800

    Fix CORS

--- 02-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ 02-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -12,18 +12,18 @@ public class RestfulWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
-	
+
 	//http://localhost:3000/ to 8080
 	//Cross Origin Requests
 	//Allow all requests only from http://localhost:3000/
-	
+
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
 					.allowedMethods("*")
-					.allowedOrigins("http://localhost:3000");
+					.allowedOrigins("*");
 			}
 		};
 	}
